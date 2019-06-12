16e0387b9fe1b4a96792a10fb99ecf5bb1f2a7e2
Fatih Aydinoglu <fatihaydinoglu@Fatihs-MacBook-Pro.local>
2019-06-12 15:33:05 +0100

    remove cra text

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <div className="App">Fatih Aydinoglu</div>
+    </>
+  )
+}
+
+export default App
