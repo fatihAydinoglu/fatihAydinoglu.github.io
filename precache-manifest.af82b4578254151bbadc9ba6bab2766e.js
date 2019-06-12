88fa98d42ca9d59dfb73ba201ccabc5c436ce630
Fatih Aydinoglu <fatihaydinoglu@Fatihs-MacBook-Pro.local>
2019-06-12 15:41:41 +0100

    Updates

---
+++ precache-manifest.af82b4578254151bbadc9ba6bab2766e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6caff593a5f7b8f8554c32b4f373b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f06f8990576244fc19af",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "ac93f79405286583cae7",
+    "url": "/static/js/2.aa087c15.chunk.js"
+  },
+  {
+    "revision": "f06f8990576244fc19af",
+    "url": "/static/js/main.8da724e5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
